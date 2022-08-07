04c98a09a404f166b1494e5715c8da1055fe57bf
Florian Truchot <florian@fantassin.fr>
2022-08-07 11:07:30 +0200

    feat: create Twenty Twenty child theme

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+add_action('wp_enqueue_scripts', 'wordcamp_lyon_add_styles');
+
+/**
+ * Enqueue parent and child styles.
+ */
+function wordcamp_lyon_add_styles(){
+    $parenthandle = 'twentytwenty-style';
+    $theme = wp_get_theme();
+
+    // Parent styles.
+    wp_enqueue_style(
+        $parenthandle,
+        get_template_directory_uri() . '/style.css',
+        array(),
+        $theme->parent()->get('Version')
+    );
+
+    // Child styles.
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_uri(),
+        array( $parenthandle )
+    );
+}
